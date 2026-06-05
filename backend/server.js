0097034430f0c697a97ded37e38153bd780e2364
aryan12191 <229527138+aryanrichhariya1234-lang@users.noreply.github.com>
2026-06-05 19:42:23 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin:
       process.env.FRONTEND_URL ||
-      "https://travel-itinerary-eosin-iota.vercel.app/",
+      "https://travel-itinerary-eosin-iota.vercel.app",
     credentials: true,
   })
 );
